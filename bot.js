a4bc47f8a6a2a1a03e2166a06852b250a697379e
iibrahimwt13 <63889403+iibrahimwt13@users.noreply.github.com>
2021-01-17 17:37:22 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,6 @@
-/* Copyright (C) 2020 İbo Usta.
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-WylineBot - İbo Usta
-*/
+
+##WylineBot - İbo Usta
+
 
 const fs = require("fs");
 const path = require("path");
